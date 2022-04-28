b23dfb79ec64c8d0672c05972b197132461f3690
yanghyeonji <yanghyeonji@naver.com>
2022-04-28 13:03:40 +0900

    statusBar commit

--- app/src/main/java/com/example/boxapplication/MainActivity.java
+++ app/src/main/java/com/example/boxapplication/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.boxapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,7 +12,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         // 왼쪽 상단에 앱 아이콘 출력
-        getSupportActionBar().setDisplayShowHomeEnabled(true);
-        getSupportActionBar().setIcon(R.drawable.ic_launcher);
+        //getSupportActionBar().setDisplayShowHomeEnabled(true);
+        //getSupportActionBar().setIcon(R.drawable.ic_launcher);
+
+        // 상태바 숨기기
+        //getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+        //        WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        // 타이틀바 숨기기
+        getSupportActionBar().hide();
+
     }
 }
